72bfff632e14e386a55889f29f0395321aca08b8
frol <frolvlad@gmail.com>
2014-08-24 23:38:08 -0700

    Added chapter #102; Switch to XeLaTeX; Change main font to Georgia; Improved parser.sh script to fetch and parse content;

--- parser/parse.sh
+++ parser/parse.sh
@@ -13,6 +13,21 @@ if [ ! -f "part_$CHAPTER_NUMBER.html" ]; then
     exit 2
 fi
 
+_PARSER_GET_CONTENT="s/^id='storytext'>(.*)/\1/"
+_PARSER_REPLACE_BR="s/<br( [^>]*|\/)?>/<\/p><p>/g"
+_PARSER_FIX_SPACES_AROUND_CLOSE_TAGS="s/ (<\/[^>]*>) *([^\"])/\1 \2/g"
+_PARSER_FIX_SPACES_AROUND_OPEN_TAGS="s/([^\"]) *(<[^/][^>]*>)  */\1 \2/g"
+_PARSER_FIX_SPACES_BEFORE_QUOTE="s/([^ >][\"])/ \1/g"
+PARSER_EXTRACT_CONTENT="$_PARSER_GET_CONTENT;$_PARSER_REPLACE_BR;$_PARSER_FIX_SPACES_AROUND_CLOSE_TAGS;$_PARSER_FIX_SPACES_AROUND_OPEN_TAGS;$_PARSER_FIX_SPACES_BEFORE_QUOTE"
+
+_PARSER_FIX_ELLIPSIS="s/\\\ldots\{\}/.../g"
+_PARSER_CONVERT_QUOTES="s/\"([^\"]+)\"/\`\`\1''/g"
+_PARSER_FIX_SPACE_BEFORE_HYPHENQUOTE="s/  *-''/~-''/g"
+_PARSER_CONVERT_DISALOG_DASHES="s/^ *-  */---\\\,/g"
+_PARSER_FIX_SPACES_WITH_PUNCTUATION="s/([-,.?!])  *''( |$)/\1''\2/g;s/  *([,.?!])/\1/g"
+_PARSER_CONVERT_DASHES="s/( |([,.?!]) ?) *-  */\2~--- /g"
+PARSER_FIX_TEX="$_PARSER_FIX_ELLIPSIS;$_PARSER_CONVERT_QUOTES;$_PARSER_FIX_SPACE_BEFORE_HYPHENQUOTE;$_PARSER_CONVERT_DISALOG_DASHES;$_PARSER_FIX_SPACES_WITH_PUNCTUATION;$_PARSER_CONVERT_DASHES"
+
 echo "\\weblink{https://www.fanfiction.net/s/5782108/$CHAPTER_NUMBER/}" > part_$CHAPTER_NUMBER.tex
 grep "<title" part_$CHAPTER_NUMBER.html | sed -r "s/.*Chapter ([0-9]*): (.*), a harry.*/\\\chapter{Chapter \1: \2}/" >> part_$CHAPTER_NUMBER.tex
-grep "nocopy" part_$CHAPTER_NUMBER.html | sed -r "s/^id='storytext'>(.*)/\1/;s/<br( [^>]*|\/)?>/<\/p><p>/g;s/ (<\/[^>]*>) */\1 /g;s/ *(<[^/][^>]*>)  */ \1/g;s/[\"]/ \"/g" | pandoc -f html -t latex | sed -r "s/\\\ldots\{\}/.../g;s/^\"/\`\`/g;s/\"/''/g;s/^ *-  */\"--* /g;s/([-,.?!])  *''/\1''/g;s/( |[,.?!] ?) *-  */\1\"--- /g" >> part_$CHAPTER_NUMBER.tex
+grep "nocopy" part_$CHAPTER_NUMBER.html | sed -r "$PARSER_EXTRACT_CONTENT" | pandoc --no-wrap -f html -t latex | sed -r "$PARSER_FIX_TEX" >> part_$CHAPTER_NUMBER.tex
